3bb14021893bcd73a28ad84cd152adcdb119639b
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-03-22 11:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	let sum = 0;
+	for (let i = 0; i < num.length; i++) {
+		for (let j = i; j < num.lenght; j++) {
+			if (num[i] + num[j] >= 0) {
+				sum = num[i] + num[j]
+			}
+		}
+	}
+	return sum;
 }
 
 module.exports = fibonacci;

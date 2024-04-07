7532304e38005473dd4132638934efcdfe71a4e5
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-07 14:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fibonacci(num) {
 // your code here
 	let sum = 0;
 	for (let i = 0; i < num.length; i++) {
-		for (let j = i; j < num.lenght; j++) {
+		for (let j = i; j < num.length; j++) {
 			if (num[i] + num[j] >= 0) {
 				sum = num[i] + num[j]
 			}
